e5badc6f473872c1a56db9ea527f290ca5d34276
shreya-rakholiya <rakholiyashreya@gmail.com>
2025-03-25 21:50:04 +0530

    populate

--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -2,26 +2,28 @@ import { FilterQuery } from "mongoose";
 import { blogModel } from "../models/blog";
 import { Iblog } from "../types/model.types";
 
-export const createBlog = async (input:FilterQuery<Iblog>) => {
+export const createBlog = async (input: FilterQuery<Iblog>) => {
   const savedBlog = await new blogModel(input).save();
   return savedBlog;
 };
-export const findAllBlog=async()=>{
-  const blogs=await blogModel.find().lean();
+export const findAllBlog = async () => {
+  const blogs = await blogModel.find().populate("image").lean();
   return blogs;
-}
-export const findBlog = async (id:any) => {
-  const blog = await blogModel.find({_id:id});
+};
+export const findBlog = async (id: any) => {
+  const blog = await blogModel.find({ _id: id }).populate("image");
   return blog;
 };
 
-export const updateBlog = async (query:FilterQuery<Iblog>, updates:Partial<Iblog>) => {
+export const updateBlog = async (
+  query: FilterQuery<Iblog>,
+  updates: Partial<Iblog>
+) => {
   const blog = await blogModel.updateMany(query, updates);
   return blog;
 };
 
-export const deleteBlog = async (id:any) => {
+export const deleteBlog = async (id: any) => {
   const blog = await blogModel.findByIdAndDelete(id);
   return blog;
 };
-
