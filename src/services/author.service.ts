ba8ab0a448e98889be005c63e1001c61a7cc2381
shreya-rakholiya <rakholiyashreya@gmail.com>
2025-03-30 15:09:37 +0530

    authorrrr

--- src/services/author.service.ts
+++ src/services/author.service.ts
@@ -1,29 +1,37 @@
-import { FilterQuery } from 'mongoose';
-import {authorModel} from '../models/author';
-import { Iauthor } from '../types/model.types';
-import { userModel } from '../models/user';
+import { FilterQuery } from "mongoose";
+import { authorModel } from "../models/author";
+import { Iauthor } from "../types/model.types";
+import { userModel } from "../models/user";
 
-export const createAuthor=async (input:FilterQuery<Iauthor>)=>{
-    const author=await userModel.create({...input,role: 'author'});
-    return author;
-}
+export const createAuthor = async (input: FilterQuery<Iauthor>) => {
+  const author = await userModel.create({ ...input, role: "author" });
+  return author;
+};
 
-export const findAuthor=async (query:FilterQuery<Iauthor>)=>{
-    const author = await userModel.findOne({...query,role: 'author'});
-    return author;
-}
+export const findAuthor = async (query: FilterQuery<Iauthor>) => {
+  const author = await userModel.findOne({ ...query, role: "author" })
+  .populate('profile');
+  return author;
+};
 
-export const findAllAuthor=async()=>{
-    const authors=await userModel.find({role:'author'});
-    return authors;
-}
+export const findAllAuthor = async () => {
+  const authors = await userModel.find({ role: "author" })
+  .populate('profile');
+  return authors;
+};
 
-export const updateAuthor=async(query:FilterQuery<Iauthor>,update:Partial<Iauthor>)=>{
-    const author = await userModel.updateOne({...query,role:'author'},update);
-    return author;
-}
+export const updateAuthor = async (
+  query: FilterQuery<Iauthor>,
+  update: Partial<Iauthor>
+) => {
+  const author = await userModel.updateOne(
+    { ...query, role: "author" },
+    update
+  );
+  return author;
+};
 
-export const deleteAuthor=async (query:FilterQuery<Iauthor>)=>{
-    const author = await userModel.deleteOne({...query,role:'author'});
-    return author;
-}
+export const deleteAuthor = async (query: FilterQuery<Iauthor>) => {
+  const author = await userModel.deleteOne({ ...query, role: "author" });
+  return author;
+};
