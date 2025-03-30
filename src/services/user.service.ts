501f627e789fadb3507459fb59186183b9ea29fc
shreya-rakholiya <rakholiyashreya@gmail.com>
2025-03-30 15:46:37 +0530

    userpop

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,74 +4,88 @@ import { Iuser } from "../types/model.types";
 import { fineModel } from "../models/fine";
 import path from "path";
 
-export const createUser=async(input:FilterQuery<Iuser>)=>{
-    const user=await new userModel(input).save();
-    return user;
-}
+export const createUser = async (input: FilterQuery<Iuser>) => {
+  const user = await new userModel(input).save();
+  return user;
+};
 
-export const findAllUser=async()=>{
-    const users=await userModel.find({role: 'user'});
-    return users;
-}
+export const findAllUser = async () => {
+  const users = await userModel
+    .find({ role: "user" })
+    .populate("profile")
+    .lean();
+  return users;
+};
 
-export const findUser=async(query:FilterQuery<Iuser>)=>{
-    console.log(query,"servicesss");
-    const user=await userModel.findOne(query).lean();
-    return user;
-}
-
-export const updateUser=async(query:FilterQuery<Iuser>,update:Partial<Iuser>)=>{
-    const user=await userModel.updateOne(query,update);
-    return user;
-}
-
-export const deleteUser=async(query:FilterQuery<Iuser>)=>{
-    const user=await userModel.deleteMany(query);
-    return user;
-}
-
-export const getProfile=async(id:string,role:string)=>{
-    const admin=await userModel.find({_id:id,role})
+export const findUser = async (query: FilterQuery<Iuser>) => {
+  console.log(query, "servicesss");
+  const user = await (await userModel.findOne(query))
     .populate('profile')
-    return admin;
-}
+  return user;
+};
 
-export const updateProfile=async(id:string,role:string,update:Partial<Iuser>)=>{
-    const admin=await userModel.updateOne({_id:id,role},update)
-    return admin;
-}
+export const updateUser = async (
+  query: FilterQuery<Iuser>,
+  update: Partial<Iuser>
+) => {
+  const user = await userModel.updateOne(query, update);
+  return user;
+};
 
-export const findBorrowedBookOfUser=async(userId:string)=>{
-    const borrowedBooks=await userModel.findById(userId)
-    .select('borrowedBooks')
-    .populate({path:'borrowedBooks',
-        match: { status: 'active' },
-        populate:{
-            path:'bookId'
-        }
+export const deleteUser = async (query: FilterQuery<Iuser>) => {
+  const user = await userModel.deleteMany(query);
+  return user;
+};
+
+export const getProfile = async (id: string, role: string) => {
+  const admin = await userModel.find({ _id: id, role }).populate("profile");
+  return admin;
+};
+
+export const updateProfile = async (
+  id: string,
+  role: string,
+  update: Partial<Iuser>
+) => {
+  const admin = await userModel.updateOne({ _id: id, role }, update);
+  return admin;
+};
+
+export const findBorrowedBookOfUser = async (userId: string) => {
+  const borrowedBooks = await userModel
+    .findById(userId)
+    .select("borrowedBooks")
+    .populate({
+      path: "borrowedBooks",
+      match: { status: "active" },
+      populate: {
+        path: "bookId",
+      },
     })
-    .lean()
-    return borrowedBooks;
-}
+    .lean();
+  return borrowedBooks;
+};
 
-export const findreturnedBorrowBookOfUser=async(userId:string)=>{
-    const borrowedBooks=await userModel.findById(userId)
-    .select('borrowedBooks')
-    .populate({path:'borrowedBooks',
-        match: { status: 'returned' },
-        populate:{
-            path:'bookId'
-        }
+export const findreturnedBorrowBookOfUser = async (userId: string) => {
+  const borrowedBooks = await userModel
+    .findById(userId)
+    .select("borrowedBooks")
+    .populate({
+      path: "borrowedBooks",
+      match: { status: "returned" },
+      populate: {
+        path: "bookId",
+      },
     })
-    .lean()
-    return borrowedBooks;
-}
+    .lean();
+  return borrowedBooks;
+};
 
-export const findFineOfUser=async(userId:string)=>{
-    const fine=await fineModel.find({user:userId})
-}
+export const findFineOfUser = async (userId: string) => {
+  const fine = await fineModel.find({ user: userId });
+};
 
-export const countUserForSpecificRole=async(type:string)=>{
-    const count=await userModel.countDocuments({role:type});
-    return count;
-}
+export const countUserForSpecificRole = async (type: string) => {
+  const count = await userModel.countDocuments({ role: type });
+  return count;
+};
