0e13142a7266e4241c1f128af71efa811f553050
shreya-rakholiya <rakholiyashreya@gmail.com>
2025-03-31 23:25:51 +0530

    review

--- src/controllers/author/review.controller.ts
+++ src/controllers/author/review.controller.ts
@@ -16,14 +16,15 @@ export const getReviewsOfBookController=async(req:Request,res:Response)=>{
 
 export const getAllBooksWithReviewsController = async (req: Request, res: Response) => {
     try {
+      const authUserId=req.authuserId
       // Since we're starting from the Book model, first get all books
-      const books = await bookModel.find();
+      const books = await bookModel.find({author:authUserId});
       
       // Map through books and add reviews to each book
       const booksWithReviews = await Promise.all(
         books.map(async (book) => {
           // Find all reviews for this book and populate user information
-          const bookReviews = await reviewModel.find({ bookId: book._id })
+          const bookReviews = await reviewModel.find({ bookId: book._id,})
             .populate('user', 'name email profilePicture')
             .lean();
           
--- src/routers/author/review.route.ts
+++ src/routers/author/review.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { validateAuthIdToken } from '../../middleware/auth';
 import { getAllBooksWithReviewsController } from '../../controllers/author/review.controller';
+import { validateAuthor } from '../../middleware/validateAuthor';
 
 const reviewRoute=express.Router();
 
 // @ts-ignore
-reviewRoute.get('/get',validateAuthIdToken,getAllBooksWithReviewsController);
+reviewRoute.get('/get',validateAuthIdToken,validateAuthor,getAllBooksWithReviewsController);
 
 export {reviewRoute}
